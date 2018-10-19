ec86592c4855f79554c26d00eec19bfb6fc05235
ence23 <44281419+ence23@users.noreply.github.com>
2018-10-19 10:26:25 +0800

    Add files via upload

---
+++ boxer.java
@@ -0,0 +1,23 @@
+package example;
+
+import dsa.SingularlyLinkedList;
+
+public class boxer {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SingularlyLinkedList<String> boxers= new SingularlyLinkedList <String>();
+		
+		boxers.addFirst("Neil");
+		boxers.addFirst("JOan");
+		boxers.addFirst("Nc");
+		boxers.addFirst("Pedro");
+		
+		System.out.println("size : " + boxers.size());
+		
+	}
+
+}
+
+
+
+	
